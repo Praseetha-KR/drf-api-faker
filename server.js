37636d82fd315de10d6052fe7cea857c9afe40e2
Praseetha-KR <praseetha04@gmail.com>
2019-04-16 20:42:39 +0530

    Rewrite specified namespaces, added getPrevNext fn

--- server.js
+++ server.js
@@ -5,12 +5,52 @@ const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults({noCors: true})
 
-function getBaseURL(req) {
+DEFAULT_LIMIT = process.env.DEFAULT_LIMIT || 10
+DEFAULT_OFFSET = process.env.DEFAULT_OFFSET || 0
+API_NAMESPACES = process.env.API_NAMESPACES || '/api/v1/*,/api/*'
+
+function getFullURL(req) {
   const root = url.format({
     protocol: req.protocol,
     host: req.get('host')
   });
-  return `${root}${req.path}`;
+  return `${root}${req.originalUrl}`;
+}
+
+function getBaseURL(req) {
+  const url = getFullURL(req) || ''
+  try {
+    return url.split('?')[0]
+  } catch(_) {}
+  return url
+}
+
+function getPrevNext(baseURL, limit, offset, count) {
+  let next = null
+  let prev = null
+  
+  if ((offset + limit) < count) {
+    next = `?limit=${limit}&offset=${offset + limit}`
+  }
+  if (offset != 0) {
+    if ((offset - limit) > 0) {
+      prev = `?limit=${limit}&offset=${offset - limit}`
+    } else {
+      prev = `?limit=${limit}`
+    }
+  }
+  return {
+    prev: prev ? (baseURL + prev) : prev, 
+    next: next ? (baseURL + next) : next
+  }
+}
+
+function rewriteAPINamespaces(namespaceCsv) {
+  const ns = namespaceCsv.replace(/\s/g, '').split(',')
+  return ns.reduce(function(acc, curr) {
+    acc[curr] = '/$1'
+    return acc
+  }, {})
 }
 
 server.use(middlewares)
@@ -23,43 +63,25 @@ server.use((req, res, next) => {
 })
 
 router.render = function (req, res) {
-  let _data = {}
   // GET
   if (req.method === 'GET') {
-    _data = res.locals.data
-    const baseURL = getBaseURL(req)
-    const count = _data.length
-    let next = null
-    let prev = null
-
     if (res.statusCode === 200) {
-      let offset = parseInt(req._query.offset, 10) || 0
-      let limit = parseInt(req._query.limit, 10) || 10
-      let paginated_data = _data.slice(offset, offset + limit)
-
-      if ((offset + limit) < count) {
-        next = `?limit=${limit}&offset=${offset + limit}`
+      let _data = res.locals.data
+      if (Array.isArray(_data)) {
+        let offset = parseInt(req._query.offset, 10) || DEFAULT_OFFSET
+        let limit = parseInt(req._query.limit, 10) || DEFAULT_LIMIT
+        let count = _data.length
+        let results = _data.slice(offset, offset + limit)
+        let {prev, next} = getPrevNext(getBaseURL(req), limit, offset, count)
+        res.jsonp({count, prev, next, results})
+      } else {
+        res.jsonp(_data)
       }
-      if (offset != 0) {
-        if ((offset - limit) > 0) {
-          prev = `?limit=${limit}&offset=${offset - limit}`
-        } else {
-          prev = `?limit=${limit}`
-        }
-      }
-
-      res.jsonp({
-        count: count,
-        prev: prev ? (baseURL + prev) : prev,
-        next: next ? (baseURL + next) : next,
-        results: paginated_data,
-      })
     }
   }
   // POST, PUT, PATCH, DELETE
   else {
-    _data = req.body
-    res.jsonp(_data)
+    res.jsonp(req.body)
   }
 }
 
@@ -70,6 +92,8 @@ server.use((req, res, next) => {
   }
   next()
 })
+
+server.use(jsonServer.rewriter(rewriteAPINamespaces(API_NAMESPACES)))
 server.use(router)
 
 server.listen(3000, () => {
